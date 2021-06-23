fba53e00df5a0e462f2e4e76730d54b2bb338637
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-06-23 00:11:31 -0300

    estilos da pagina Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,24 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logo from '../assets/images/logo.svg';
 import googleIconImage from '../assets/images/google-icon.svg';
-
+import '../styles/auth.scss';
 
 export function Home(){
     return(
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo real</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logo} alt="Logo da letmeask" />
-                    <button >
+                    <button className="create-room">
                         <img src={googleIconImage} alt="logo da google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className="separator">ou entre em uma sala</div>
                     <form action="">
                         <input 
                         type="text" 
