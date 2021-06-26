5bd4372dba391e65b82e51080ee6e48916236dd7
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-06-26 17:48:58 -0300

    sala do admin e estilizacao

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -20,7 +20,7 @@ type RoomParams ={
 
 
 
-export function Room() {
+export function AdminRoom() {
     const {user} = useAuth();
     const params = useParams<RoomParams>();
 
@@ -62,7 +62,11 @@ export function Room() {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="letmeask" />
-                    <RoomCode code={params.id}/>
+                    <div>
+                        <RoomCode code={params.id}/>
+                        <Button isOutlined children="Encerrar sala"/>
+                    </div>
+
                 </div>
             </header>
             <main className="main-content">
@@ -70,29 +74,6 @@ export function Room() {
                     <h1>Sala {title}</h1>
                     {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
                 </div>
-                <form onSubmit={handleSendQuestion}>
-                    <textarea 
-                        placeholder="O que você quer perguntar?"
-                        onChange={event=> setNewQuestion(event.target.value)}
-                        value={newQuestion}
-                    />
-
-                    <div className="form-footer">
-                        {
-                            user ? (
-                                <div className="user-info">
-                                    <img src={user.avatar} alt={user.name} />
-                                    <span>{user.name}</span>
-                                </div>
-                            ) : (
-                                <span>Para enviar uma pergunta <button>faça seu login</button></span>
-                            ) 
-                        }
-                        <Button type="submit" disabled={!user} children="Enviar pergunta" />
-                    </div>
-                </form>
-
-
 
                 <div className="question-list">
                     {questions.map(question =>{
